1f1cdea684e2d2405e9023b0f49694a674c9558a
Megan Huang <megan@10-18-117-150.dynapool.wireless.nyu.edu>
2022-11-10 13:26:32 -0500

    final tweaks

--- script.js
+++ script.js
@@ -53,31 +53,47 @@ function generate(input){
     outputText.innerText = textArray[3];
     outputImage.src = imgArray[3];
   } else {
-    outputText.innerText = "try again";
+    outputText.innerText = "try again :( with a number from 0-3!";
   }
 }
 
 function restyle (input){
   if (input == 0){
+    document.body.style.borderStyle = "dotted";
+    document.body.style.borderWidth = "1vw";
+    document.body.style.borderColor = "#8f0110";
     document.body.style.backgroundColor = "#ffedde";
     outputText.style.color = "#8f0110";
+    heading.style.color = "#8f0110";
   }
   else if (input == 1){
+    document.body.style.borderStyle = "double";
+    document.body.style.borderWidth = "0.5vw";
+    document.body.style.borderColor = "white";
     document.body.style.backgroundColor = "#7a2626";
-    outputText.style.color = "black";
+    outputText.style.color = "#301102";
     heading.style.color = "black";
 
   }
   else if (input == 2){
+    document.body.style.borderStyle = "dashed";
+    document.body.style.borderWidth = "0.5vw";
+    document.body.style.borderColor = "#5c2200";
     document.body.style.backgroundColor = "#def5ff";
     outputText.style.color = "#e0a046";
     heading.style.color = "#e0a046";
 
   }
   else if (input == 3){
+    document.body.style.borderStyle = "solid";
+    document.body.style.borderWidth = "0.8vw";
+    document.body.style.borderColor = "white";
     document.body.style.backgroundColor = "#ecd4ff";
     outputText.style.color = "#e84a87";
     heading.style.color = "#e84a87";
+
+  } else{
+    outputText.style.color = "red";
 
   }
 
